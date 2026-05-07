4627f35dcdfc977c4c6b917f263b16e535c2097a
damiaoregis <damiao.v.serrana@gmail.com>
2026-05-07 15:20:23 -0300

    Consulta siples com query Methods

--- src/main/java/com/damiaoregis/workshopmongo/resources/PostResource.java
+++ src/main/java/com/damiaoregis/workshopmongo/resources/PostResource.java
@@ -1,16 +1,20 @@
 package com.damiaoregis.workshopmongo.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import com.damiaoregis.workshopmongo.domain.Post;
+import com.damiaoregis.workshopmongo.resources.util.URL;
 import com.damiaoregis.workshopmongo.services.PostService;
 
 
@@ -26,6 +30,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value= "text" )String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitleContainingIgnoreCase(text);
+        return ResponseEntity.ok().body(list);
     }
     
 }
